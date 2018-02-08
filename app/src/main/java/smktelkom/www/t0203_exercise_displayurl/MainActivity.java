fde66cf4f494415d57943aab08d199c9cffd0268
M12RjUL14n <julian_resta_25rpl@student.smktelkom-mlg.sch.id>
2018-02-08 23:22:05 +0700

    Julian

---
+++ app/src/main/java/smktelkom/www/t0203_exercise_displayurl/MainActivity.java
@@ -0,0 +1,57 @@
+package smktelkom.www.t0203_exercise_displayurl;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText mSearchBoxEditText;
+
+    private TextView mUrlDisplayTextView;
+
+    private TextView mSearchResultsTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSearchBoxEditText = findViewById(R.id.et_search_box);
+
+        mUrlDisplayTextView = findViewById(R.id.tv_url_display);
+        mSearchResultsTextView = findViewById(R.id.tv_github_search_results_json);
+    }
+
+    // TODO (2) Create a method called makeGithubSearchQuery
+    // TODO (3) Within this method, build the URL with the text from the EditText and set the built URL to the TextView
+
+    private void makeGithubSearchQuery() {
+        String githubQuery = mSearchBoxEditText.getText().toString();
+        URL githubSearchUrl = NetworkUtils.buildUrl(githubQuery);
+        mUrlDisplayTextView.setText(githubSearchUrl.toString());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.action_search) {
+            // TODO (4) Remove the Toast message when the search menu item is clicked
+            // TODO (5) Call makeGithubSearchQuery when the search menu item is clicked
+            makeGithubSearchQuery();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+
